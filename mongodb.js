0fd5a2dbe14ede38938f0b5e76d8149901023447
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2023-04-29 14:37:20 -0400

    created a port

--- mongodb.js
+++ mongodb.js
@@ -1,2 +1,17 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
+
+
+const app = express()
+const port = 4005
+
+app.use(cors()); 
+app.use(express.json())
+
+app.get("/", (req, res) => {
+    res.status(200).send("hello bridgette, great to see this is working")
+})
+
+app.listen(port, (req, res) => {
+    console.log(`ready to listen on http://localhost:` + port)
+})
